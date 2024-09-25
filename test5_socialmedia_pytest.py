f867d7a3dccba69014da2866452ec4459c651a1d
Anca_Sab <asabadeanu@gmail.com>
2024-09-25 11:19:12 +0300

    test contact finalizat

--- test5_socialmedia_pytest.py
+++ test5_socialmedia_pytest.py
@@ -6,11 +6,6 @@ import time
 from selenium.webdriver.chrome.options import Options
 
 
-driver = webdriver.Chrome()
-chrome_options = Options()
-chrome_options.add_argument("--disable-search-engine-choice-screen")
-driver = webdriver.Chrome(options=chrome_options)
-
 @pytest.fixture(scope="function")
 def setup_teardown():
     # Setup WebDriver
@@ -33,10 +28,10 @@ def test_deschidere_linkedin(setup_teardown):
     time.sleep(3)
 
     linkedin_message = driver.find_element(By.CSS_SELECTOR, "button.artdeco-global-alert-action")
-    time.sleep(5)
+    # time.sleep(3)
     linkedin_message.text
     assert linkedin_message.text == "Accept"
-    time.sleep(5)
+    # time.sleep(3)
 
 
 
@@ -52,9 +47,9 @@ def test_accesare_facebook(setup_teardown):
     time.sleep(3)
 
     facebook_message = driver.find_element(By.CSS_SELECTOR, "body > div.__fb-light-mode.x1n2onr6.x1vjfegm > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x1exxf4d.x13fuv20.x178xt8z.x1l90r2v.x1pi30zi.x1swvt13 > div > div:nth-child(2) > div.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x87ps6o.x1lku1pv.x1a2a7pz.x9f619.x3nfvp2.xdt5ytf.xl56j7k.x1n2onr6.xh8yej3 > div > div.x6s0dn4.x78zum5.xl56j7k.x1608yet.xljgi0e.x1e0frkt")
-    time.sleep(5)
+    # time.sleep(3)
     assert facebook_message.text == "Allow all cookies"
-    time.sleep(5)
+    # time.sleep(3)
 
     
 
---
+++ test6_contact_pytest.py
@@ -0,0 +1,163 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import datetime
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(scope="function")
+def setup_teardown():
+    # Setup WebDriver
+    # driver = webdriver.Chrome()
+
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-search-engine-choice-screen")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://ursus-breweries.ro")
+
+    # Accept cookie consent dialog (adjust timeout as needed)
+    time.sleep(10)
+    COOKIE_ID = "CybotCookiebotDialogBodyLevelButtonAcceptWrapper"
+    driver.find_element(By.ID, COOKIE_ID).click()
+
+    # Improved approach using WebDriverWait
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.invisibility_of_element_located((By.ID, "CybotCookiebotDialog")))
+
+    # def access_marci(self):
+    driver.find_element(By.CSS_SELECTOR, ".button-menu.button-menu--js.aos-init.aos-animate").click()
+    
+    time.sleep(3)
+    driver.find_element(By.LINK_TEXT, "Contact").click()
+    time.sleep(10)
+    yield driver
+    # Teardown after test
+    driver.quit()
+
+
+
+def test_formular_contact_complete(setup_teardown):
+    driver = setup_teardown
+    time.sleep(3)
+    driver.find_element(By.XPATH, "/html/body/div[1]/section[1]/div[2]/div[2]/span").click()
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0,600 + window.scrollY);")
+    driver.find_element(By.CSS_SELECTOR, "#wpcf7-f2652-o1 > form > h3")
+    time.sleep(3)
+
+    input_first_name = driver.find_element(By.NAME, "field-first_name")
+    input_first_name.send_keys("Popescu")
+    input_first_name.send_keys(Keys.TAB)
+    time.sleep(3)
+
+    input_second_name = driver.find_element(By.NAME, "field-last_name")
+    input_second_name.send_keys("Marin")
+    input_second_name.send_keys(Keys.TAB)
+    time.sleep(3)
+
+    input_email = driver.find_element(By.NAME, "field-email")
+    input_email.send_keys("Marin_Popescu@yaho.com")
+    input_email.send_keys(Keys.TAB)
+    time.sleep(3)
+
+    input_message = driver.find_element(By.NAME, "field-message")
+    input_message.send_keys("Mesaj test")
+    time.sleep(3)
+
+    btn_trimite = driver.find_element(By.CLASS_NAME, "btn--submit-ro")
+    btn_trimite.click()
+    time.sleep(10)
+
+    mesaj_confirmare = driver.find_element(By.CLASS_NAME, "wpcf7-response-output")
+    time.sleep(10)
+    assert mesaj_confirmare.text == 'Mesajul tău a fost trimis.'
+
+def test_formular_contact_empty(setup_teardown):
+    driver = setup_teardown
+    time.sleep(3)
+    driver.find_element(By.XPATH, "/html/body/div[1]/section[1]/div[2]/div[2]/span").click()
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0,600 + window.scrollY);")
+    driver.find_element(By.CSS_SELECTOR, "#wpcf7-f2652-o1 > form > h3")
+    time.sleep(3)
+
+    input_first_name = driver.find_element(By.NAME, "field-first_name")
+    input_first_name.send_keys("")
+    input_first_name.send_keys(Keys.TAB)
+    time.sleep(3)
+
+    input_second_name = driver.find_element(By.NAME, "field-last_name")
+    input_second_name.send_keys("")
+    input_second_name.send_keys(Keys.TAB)
+    time.sleep(3)
+
+    input_email = driver.find_element(By.NAME, "field-email")
+    input_email.send_keys("")
+    input_email.send_keys(Keys.TAB)
+    time.sleep(3)
+
+    input_message = driver.find_element(By.NAME, "field-message")
+    input_message.send_keys("")
+    time.sleep(3)
+
+    btn_trimite = driver.find_element(By.CLASS_NAME, "btn--submit-ro")
+    btn_trimite.click()
+    time.sleep(10)
+
+    lista_mesaje_eroare = driver.find_elements(By.CLASS_NAME, "wpcf7-not-valid-tip")
+    for i in range(len(lista_mesaje_eroare)):
+        assert lista_mesaje_eroare[i].text == 'Câmpul este obligatoriu.'
+    
+    mesaj_confirmare = driver.find_element(By.CLASS_NAME, "wpcf7-response-output")
+    time.sleep(10)
+    assert mesaj_confirmare.text == 'Unul sau mai multe câmpuri au o eroare. Te rog să verifici și să încerci din nou.'
+
+def test_formular_email(setup_teardown):
+    driver = setup_teardown
+    time.sleep(3)
+    driver.find_element(By.XPATH, "/html/body/div[1]/section[1]/div[2]/div[2]/span").click()
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0,600 + window.scrollY);")
+    driver.find_element(By.CSS_SELECTOR, "#wpcf7-f2652-o1 > form > h3")
+    time.sleep(3)
+
+    input_first_name = driver.find_element(By.NAME, "field-first_name")
+    input_first_name.send_keys("jgkjhgkjh")
+    input_first_name.send_keys(Keys.TAB)
+    time.sleep(3)
+
+    input_second_name = driver.find_element(By.NAME, "field-last_name")
+    input_second_name.send_keys("dfhgjhvjh")
+    input_second_name.send_keys(Keys.TAB)
+    time.sleep(3)
+
+    input_email = driver.find_element(By.NAME, "field-email")
+    input_email.send_keys("aruifh")
+    input_email.send_keys(Keys.ENTER)
+    time.sleep(3)
+
+    input_message = driver.find_element(By.NAME, "field-message")
+    input_message.send_keys("email test")
+    time.sleep(3)
+
+    btn_trimite = driver.find_element(By.CLASS_NAME, "btn--submit-ro")
+    btn_trimite.click()
+    time.sleep(10)
+
+    mesaj_eroare_email = driver.find_element(By.CLASS_NAME, "wpcf7-not-valid-tip")
+    time.sleep(10)
+    assert mesaj_eroare_email.text == 'Adresa de email introdusă este invalidă.'
+
+    mesaj_confirmare = driver.find_element(By.CLASS_NAME, "wpcf7-response-output")
+    time.sleep(10)
+    assert mesaj_confirmare.text == 'Unul sau mai multe câmpuri au o eroare. Te rog să verifici și să încerci din nou.'
+
+
+ 
+
+  
+
+
